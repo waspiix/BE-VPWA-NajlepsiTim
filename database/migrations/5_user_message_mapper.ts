87e8ed0db51b244ba29e1a99026a4af519f93c40
waspiix <t.horan8777@gmail.com>
2025-10-13 15:11:57 +0200

    fixed user_message_mapper migration

--- database/migrations/5_user_message_mapper.ts
+++ database/migrations/5_user_message_mapper.ts
@@ -1,24 +1,30 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'user_channel_mapper'
+  protected tableName = 'user_message_mapper'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
-      table.integer('user_id').unsigned().notNullable()
-      table.integer('channel_id').unsigned().notNullable()
-      table.boolean('owner').defaultTo(false).notNullable()
 
-      table.timestamp('joined_at', { useTz: true }).defaultTo(this.now())
-      table.timestamp('created_at', { useTz: true })
+      table.integer('user_id').unsigned().notNullable()
+      table.integer('message_id').unsigned().notNullable()
+      table.integer('channel_id').unsigned().notNullable()
+
+      // Ak je DM = true → správa je prijatá niekým (priama správa)
+      // Ak je DM = false → správa bola niekým poslaná (v kanáli)
+      table.boolean('dm').defaultTo(false).notNullable()
+
+      table.timestamp('created_at', { useTz: true }).defaultTo(this.now())
       table.timestamp('updated_at', { useTz: true })
 
-      // FKs
+      // Foreign keys
       table.foreign('user_id').references('id').inTable('users').onDelete('CASCADE')
+      table.foreign('message_id').references('id').inTable('messages').onDelete('CASCADE')
       table.foreign('channel_id').references('id').inTable('channels').onDelete('CASCADE')
 
-      table.unique(['user_id', 'channel_id'])
+      // Unique combination (aby jedna správa nemala duplicitné mapovanie pre rovnakého usera a kanál)
+      table.unique(['user_id', 'message_id', 'channel_id'])
     })
   }
 
